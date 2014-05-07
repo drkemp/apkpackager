df2409fe6f6df1b6304c94d076c8749efc61efc0
David Kemp <drkemp@chromium.org>
2014-05-07 10:08:13 -0400

    cleanup of plugin to begin testing

--- apkpackager.js
+++ apkpackager.js
@@ -1,28 +1,31 @@
 
+var exec = cordova.require('cordova/exec');
 
-function pkgSuccess( apkpath ) {
+
+module.exports.makeapk = function(success,failure) {
+
+  function pkgSuccess( apkpath ) {
+     if(success) success();
+  }
+
+  function pkgFail(msg) {
+    console.log('Error: ' + msg);
+    if(failure) failure(msg);
+  }
+
+  function onInitFs(fs) {
+
+    // need a native compatible absolute path that ends with /
+    var workdir = 'where is the dummy stuff and APK';
+    var wwwdir = 'where is your www';
+    var resdir = 'where is your res';
+    var publicKeyURL = "file://"+workdir+"pub.x509.pem";
+    var privateKeyURL = "file://"+workdir+"pk8p.pk8";
+    var passwd="android";      // password for private key
+    exec(pkgSuccess, pkgFail, 'APKPackager', 'package', [wwwdir, resdir, workdir, publicKeyURL, privateKeyURL, passwd]);
+  }
+
+  window.requestFileSystem(window.PERSISTENT, 20*1024*1024, onInitFs, pkgFail);
 
 }
-
-function pkgFail(msg) {
-}
-
-function onInitFs(fs) {
-
-  // need a native compatible absolute path that ends with /
-  var workdir = 'where is the dummy stuff and APK';
-  var wwwdir = 'where is your www';
-  var publicKeyURL = "file://"+workdir+"pub.x509.pem";
-  var privateKeyUrl = "file://"+workdir+"pk8p.pk8");
-  var passwd="android";      // password for private key
-  exec(pkgSuccess, pkgFail, APKPackager, 'package', [wwwdir, workdir, publicKeyURL, privateKeyURL, passwd]);
-}
-
-function errorHander(msg) {
-
-  console.log('Error: ' + msg);
-}
-
-window.requestFileSystem(window.PERMANENT, 20*1024*1024, onInitFs, errorHandler);
-
 
